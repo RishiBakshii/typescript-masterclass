a2b8759a3135157c789c9300be0610bb8a9d9c87
rishibakshi <guitarguyrishi@gmail.com>
2024-02-22 21:41:11 +0530

    add arrays

---
+++ 8) Arrays/index.ts
@@ -0,0 +1,25 @@
+const superHeros: string[] = []
+const heroPower: number[] = []
+
+// we can also define like this
+const heroFans:Array<number>  = []
+
+type User={
+    name:string,
+    isActive:boolean
+}
+
+const allUsers:Array<User> = []
+
+
+superHeros.push("spiderman")
+heroPower.push(2)
+
+const mlModel:number[][] = [
+    [255,255,255],
+    [121,255,231],
+]
+
+allUsers.push({name:"rishi",isActive:true})
+allUsers.push({name:"carlos",isActive:true})
+allUsers
