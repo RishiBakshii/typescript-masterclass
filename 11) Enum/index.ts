2674fddcc5fddc015b4240afb3e6c765f3029d93
rishibakshi <guitarguyrishi@gmail.com>
2024-02-23 21:02:19 +0530

    Update enum.ts

--- 11) Enum/index.ts
+++ 11) Enum/index.ts
@@ -6,7 +6,7 @@
 
 // but if we assign a string value then we have to set the value(it can be number or string) for each variable because just like numbers strings cannot be assingned in an incremental order
 
-//* note: if we dont want that enum should produce a lot of js code then we can just simply add "const" keyword before enum, so we can right it like 
+//* note: if we dont want that enum should produce a lot of js code then we can just simply add "const" keyword before enum, so we can right it like 'const enum
 enum SeatChoice{
     AISLE='aisle',
     MIDDLE=1,
