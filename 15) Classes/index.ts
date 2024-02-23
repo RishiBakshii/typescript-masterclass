5c609f88ac700ec27f4abfa8b233c5de6088c99b
rishibakshi <guitarguyrishi@gmail.com>
2024-02-24 00:01:48 +0530

    Add classes

---
+++ 15) Classes/index.ts
@@ -0,0 +1,17 @@
+class User{
+
+    email:string
+    name:string
+    readonly city:string='default'
+
+    constructor(email:string,name:string){
+        this.email=email
+        this.name=name
+
+    }
+}
+
+const rishi = new User('r@rgmail.com','rishibakshi')
+console.log(rishi.city);
+
+export {}
