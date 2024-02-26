9fbfa89ecd306a58e22d58a3ccd7e7696582e030
rishibakshi <guitarguyrishi@gmail.com>
2024-02-26 16:44:47 +0530

    update Discriminated union and Exhastiveness checking with never

--- 27) Discriminated union and Exhastiveness checking with never/index.ts
+++ 27) Discriminated union and Exhastiveness checking with never/index.ts
@@ -25,20 +25,24 @@ function getTrueShape(shape:Shape){
         return Math.PI * shape.radius ** 2
     }
 
-    return shape.side * shape.side
+
 }
 
 
 //* The Never Type
 function getArea(shape:Shape){
     switch(shape.kind){
+
         case "circle":
             return 'this is a circle'
         case "sqaure":
             return "this is a sqaure"
         case "rectangle":
             return 'this is a rectangle'
-            
+        
+        //* the documentation says that we should always have a default case with type never on to whatever shape you are defining and you should return that
+
+        //* as default case with type never is never supposed to run so whenever my default case gets hit it will give me an error and we can handle the case that is not being handled
         default:
             const _defaultForShape:never= shape
             return _defaultForShape
